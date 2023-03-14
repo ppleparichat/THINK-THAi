86b4ad62a84c0238b982268e3c0003c504c0769b
Parichat Rodprasert <112320741+ppleparichat@users.noreply.github.com>
2023-03-14 19:46:16 +0100

    create THINK-THAi web with Mobile

---
+++ app.js
@@ -0,0 +1,8 @@
+function myNav() {
+    let nav = document.getElementById("myLinks");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+  }
