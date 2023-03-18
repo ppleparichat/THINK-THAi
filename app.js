b2ef59052e5064b3cec7d9e71fdedb6a9a3b4ad0
Parichat <ppleparichat@gmail.com>
2023-03-18 19:54:23 +0100

    update Nav THINK THAi

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const navBtn = document.getElementById("nav-click");
-// const closeBtn = document.querySelector(".nav-close");
+const closeBtn = document.getElementById("nav-close");
 const navPopup = document.querySelector(".mobile-container");
 
 
 navBtn.addEventListener("click", () => {
-  const navShow = navPopup.classList.add("show")
+  console.log("open")
+   navPopup.classList.add("show")
 });
 
-// closeBtn.addEventListener("click", () => {
-//   navPopup.classList.remove("show")
-// });
-
-
-
-
+closeBtn.addEventListener("click", ()=>{
+  console.log('close')
+  navPopup.classList.remove("show")
+})
